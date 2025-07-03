ed0ecfd30bc87b52141b8165d7f585d248c84f88
Ayush Kumar <aayushkumar1228@gmail.com>
2025-07-04 04:40:01 +0530

    Implement Sign in Feature using clerk Auth & Implement Subscription Features using Clerk Billing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
+import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 import NavItems from './NavItems'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
     return (
@@ -11,9 +12,16 @@ const Navbar = () => {
                     <Image src='/images/logo.svg' alt='Learnova' height={44} width={46} />
                 </div>
             </Link>
-            <div className='flex items-center gap-8'>
+            <div className="flex items-center gap-8">
                 <NavItems />
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     )
