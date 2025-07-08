4379a50607fc6c27ae10b8800cf0a3b688900a09
Ayush Kumar <aayushkumar1228@gmail.com>
2025-07-09 03:25:18 +0530

    add Vapi AI and make session UI functional

--- app/page.tsx
+++ app/page.tsx
@@ -2,42 +2,32 @@ import React from 'react'
 import CTA from '@/components/CTA'
 import CompanionCard from '@/components/CompanionCard'
 import CompanionList from '@/components/CompanionList'
-import { recentSessions } from '@/constants'
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions'
+import { getSubjectColor } from '@/lib/utils'
 
-const Page = () => {
+const Page = async () => {
+
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <main>
       <h1 className='text-2xl'>Popular Companions</h1>
       <section className='home-section'>
-        <CompanionCard
-          id='1'
-          name='Neura the Brainy Explorer'
-          topic='Neural Network of the Brain'
-          subject='Science'
-          duration={45}
-          color='#E5D0FF'
-        />
-        <CompanionCard
-          id='2'
-          name='Countsy the Number Wizard'
-          topic='Derivatives & Integrals'
-          subject='Maths'
-          duration={45}
-          color='#FFDA6E'
-        />
-        <CompanionCard
-          id='3'
-          name='Verba the Vocabulary Builder'
-          topic='English Literature '
-          subject='Language'
-          duration={45}
-          color='#BDE7FF'
-        />
+        {
+          companions.map((companion) => (
+            <CompanionCard
+              key={companion.id}
+              {...companion}
+              color={getSubjectColor(companion.subject)}
+            />
+          ))
+        }
       </section>
       <section className='home-section mb-5'>
         <CompanionList
           title="Recently Completed Session"
-          companions={recentSessions}
+          companions={recentSessionsCompanions}
           classNames='w-2/3 max-lg:w-full'
         />
         <CTA />
